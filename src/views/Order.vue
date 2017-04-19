183fc76be103aba9f13c7489c7f49e4c39fff023
姗姗张 <zhangshanshan@nonobank.com>
2017-04-19 12:00:11 +0800

    add box ui

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 	<section id="Order">
 		<city></city>
 		<cinemaArea></cinemaArea>
@@ -35,4 +35,59 @@ export default{
 
 <style>  
 </style>
+ -->
+<template>
+  <section>
+    <mt-header fixed
+               title="使用中的衣箱">
+        <div slot="left">
+         <mt-button v-link="'/'" ><</mt-button> 
+         </div>
+    </mt-header>
+    <section>
+      <
+    </section>
+      <div class="fixedBottom wp100  bgw">   
+       <div class="flexBox flex-row flex-main-center">
+        <router-link :to="{ name: 'order'}">
+          <mt-button size="normal" class=""  @click="addPro()">寄这个衣箱给我（1/3）</mt-button>
+          </router-link>
+       </div>         
+             
+      </div>
+  
+  </section>
+</template>
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
 
+  },
+  methods: {
+    ...mapMutations([
+  
+    ]),
+    requestData(url, fn) {
+      this.pushLoadStack()
+      this.$http.get(url).then((res) => {
+        this.completeLoad()
+        fn(res)
+      })
+    },
+
+  },
+  created() {
+
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/box.vue
+++ src/views/box.vue
@@ -1,93 +1,87 @@
 <template>
   <section>
-    <mt-header fixed
+    <mt-header 
                title="使用中的衣箱">
-      <div slot="left">
-      
-      </div>
+        <div slot="left">
+         <mt-button v-link="'/'" ><</mt-button> 
+         </div>
     </mt-header>
+
+
+<mt-navbar class="page-part p10 "  v-model="selected">
+  <mt-tab-item class="f16" id="1">使用中的衣箱</mt-tab-item>
+  <mt-tab-item class="f16" id="2">礼服订单</mt-tab-item>
+</mt-navbar>
+
+<!-- tab-container -->
+<mt-tab-container v-model="selected" class="bgw p10">
+  <mt-tab-container-item id="1">
+     <section class="">
+      <div class="">衣服状态：未寄出</div>
+      <div class="boxContain flexBox flex-col">
+         <div class="box ">
+           <div class="hasPro bgw flexBox flex-row flex-main-start">
+               <img src="../assets/images/img.png"></img>
+               <div class="flexBox flex-col ">
+                   <div class="pl10">李维斯(Levi’s)女士休闲群装 #Medium Stonewash</div>
+                   <div class="gray f12 p10" >颜色分类:黄色,尺码:s </div>
+               </div>
+           </div>
+           
+           
+         </div>
+         <div class="box flexBox flex-main-center ">
+             <div class="hasPro bgw flexBox flex-row flex-main-start">
+               <img src="../assets/images/img.png"></img>
+               <div class="flexBox flex-col ">
+                   <div class="pl10">李维斯(Levi’s)女士休闲群装 #Medium Stonewash</div>
+                   <div class="gray f12 p10" >颜色分类:黄色,尺码:s </div>
+               </div>
+           </div>
+         </div>
+          <div class="box flexBox flex-main-center ">
+           
+         </div>
+      </div>
+</section>
+  </mt-tab-container-item>
+  <mt-tab-container-item id="2">
+   礼服
+  </mt-tab-container-item>
+ 
+</mt-tab-container>
+
+
+
+  
+<div class="f12 gray p20 th">查看历史记录>></div>
+
+
       <div class="fixedBottom wp100  bgw">   
-       <div class="flexBox flex-row flex-main-center">
-        <router-link :to="{ name: 'order'}">
-          <mt-button size="normal" class=""  @click="addPro()">寄这个衣箱给我（1/3）</mt-button>
-          </router-link>
-       </div>         
-             
-        </div>
+         <div class="flexBox flex-row flex-main-center">
+          <router-link :to="{ name: 'order'}">
+            <mt-button size="normal">寄这个衣箱给我（1/3）</mt-button>
+            </router-link>
+         </div>                      
+      </div>
   
   </section>
 </template>
 <script>
 import { mapGetters, mapMutations } from 'vuex'
-import { swiper, hot } from '../components/'
 export default {
   data() {
     return {
-      moveDistance: '5%',
-      imgs: [],
-      loaingLists: [],
-      selnav: true,
-      clickLoadStatus: 'start',
-      offset: 0,
-      limit: 2,
-      total: 0,
-      hotLists: [],
+      selected:'1',
     }
   },
   components: {
-    swiper,
-    hot,
+
   },
   methods: {
     ...mapMutations([
-      'pushLoadStack',
-      'completeLoad',
-      'showCityList',
-      'pushComingList'
+  
     ]),
-    moveTab(event) {
-      event.target.getAttribute('hot') ? this.selectHotTab() : this.slectComingTab()
-    },
-    selectHotTab() {
-      this.selnav = true
-      this.moveDistance = '5%'
-      this.pushComingList({ lists: [] })
-    },
-    slectComingTab() {
-      this.selnav = false
-      this.moveDistance = '55%'
-      this.pushComingList({ lists: this.loaingLists })
-    },
-    getWeekDay(num) {
-      let weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
-      return weeks[num]
-    },
-    clickLoadMore() {
-      if (this.clickLoadStatus != 'complete') {
-        this.clickLoadStatus = 'loading'
-        setTimeout(() => {
-          this.$http.get(`/movie/coming/?limit=${this.limit}&offset=${this.offset}`).then((response) => {
-            let data = response.data
-            let lists = data.data.data.returnValue
-            this.loaingLists = this.loaingLists.concat(lists)
-            //模拟索引数据的id号
-            this.loaingLists.forEach((item, index) => {
-              item.mID = index
-            })
-            this.pushComingList({ lists: this.loaingLists })
-            this.comingLists = this.sortComingData(this.loaingLists)
-            this.offset = this.offset + this.limit
-            if (this.offset < this.total) {
-              this.clickLoadStatus = 'start'
-            } else {
-              this.clickLoadStatus = 'complete'
-            }
-          })
-
-
-        }, 500)
-      }
-    },
     requestData(url, fn) {
       this.pushLoadStack()
       this.$http.get(url).then((res) => {
@@ -95,145 +89,38 @@ export default {
         fn(res)
       })
     },
-    sortComingData(lists) {
-      let comingLists = []
-      lists.forEach((item) => {
-        let hasItem = false
-        for (let i = 0; i < comingLists.length; i++) {
-          if (item.openTime === comingLists[i].openTime) {
-            comingLists[i].movies.push(item)
-            hasItem = true
-            break
-          }
-        }
-        if (!hasItem) {
-          let comingItem = {
-            openTime: '',
-            day: '',
-            movies: []
-          }
-          comingItem.openTime = item.openTime
-          comingItem.day = this.getWeekDay(new Date(item.openTime).getDay())
-          comingItem.movies.push(item)
-          comingLists.push(comingItem)
-        }
-      })
-      return comingLists
-    }
+
   },
   created() {
-    this.pushComingList({ lists: [] })
-    this.requestData('http://rapapi.org/mockjsdata/17098/appMainBody/l', (response) => {
-      let data = response.data
-      this.imgs = data.data.data.returnValue
 
-    })
-    this.requestData('http://rapapi.org/mockjsdata/17098/appMainBody/l', (response) => {
-      let data = response.data
-      this.hotLists = data.data.data.returnValue
-    })
 
-    // this.requestData(`/movie/coming/?limit=${this.limit}&offset=${this.offset}`, (response) => {
-    //   let data = response.data
-    //   let lists = data.data.data.returnValue
-    //   //模拟索引数据的id号
-    //   lists.forEach((item, index) => {
-    //     item.mID = index  
-    //   })
-    //   this.loaingLists = lists
-    //   this.total = data.total
-    //   this.comingLists = this.sortComingData(lists)
-    //   this.offset = this.offset + this.limit
-    // })
   }
 }
 </script>
 
 <style>
-
-.selnav {
-  color: #ff4d64;
+.mint-navbar .mint-tab-item.is-selected {
+    border-bottom: 3px solid #FF3F71 !important;
+    color: #FF3F71 !important;;
+    margin-bottom: -1px !important;;
 }
-
-.selnav img {
-  height: 230px;
+.box{
+  
+    height: 90px;
+    background: #fff url('../assets/images/addboxbg.png') center no-repeat;
+    background-size: 100%;
+    margin: 10px 5px;
 }
+.hasPro{
+  height: 100px;
 
-.city {
-  width: 35%;
-  height: 100%;
-  line-height: 40px;
-  text-align: center;
-  display: inline-block;
 }
-
-.city-arrow-icon {
-  margin-left: 4px;
-  width: 12px;
-  height: 12px;
-  display: inline-block;
+.hasPro img{
+  height: 80px;
+  width: 80px
 }
-
-.city-arrow-icon:after {
-  content: "";
-  display: block;
-  width: 6px;
-  height: 6px;
-  margin-left: 0;
-  border: 1px solid #50505a;
-  border-top: 0 none;
-  border-left: 0 none;
-  margin-left: 2px;
-  -webkit-transform: rotate(45deg);
-  transform: rotate(45deg);
-}
-
-.content {
-  margin-top: 44px;
-  margin-bottom: 58px;
-}
-
-.sel-lists {
-  width: 65%;
-  display: -moz-box;
-  display: -webkit-box;
-  display: box;
-}
-
-.sel-lists div {
-  -moz-box-flex: 1.0;
-  -webkit-box-flex: 1.0;
-  line-height: 40px;
-  text-align: center;
-}
-
-.move {
-  display: inline-block;
-  bottom: 0px;
-  width: 40%;
-  border-bottom: 3px solid #ff4d64;
-  -webkit-transition: left 0.7s ease-in-out;
-  transition: left 0.7s ease-in-out;
-}
-
-.click-load-more {
-  height: 42px;
-  line-height: 42px;
-  text-align: center;
-  color: #333;
-  font-size: 15px;
-  background-color: #fff;
-  margin: 5px 0 8px;
-}
-
-.loading-icon span {
-  vertical-align: middle;
-}
-
-.labelDecribe {
-  height: 40px;
-  width: 100%;
-  background: #fff url('../assets/images/describe.png') center no-repeat;
-  background-size: 100%;
-}
+  .mint-cell{
+    height: 40px !important;;
+    min-height:40px !important;
+  }
 </style>
