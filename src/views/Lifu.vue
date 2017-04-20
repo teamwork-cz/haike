a341ef3e68ca5005276155fcd109aa6b168725cd
cao.huanan <cao.huanan@opg.cn>
2017-04-20 16:32:22 +0800

    修改嗨选页面，包月，礼服

--- src/views/Lifu.vue
+++ src/views/Lifu.vue
@@ -5,17 +5,21 @@
          infinite-scroll-distance="10">
     
         <div class="flex1"
-             v-for="item in byLists">
-            <router-link :to="{ name: 'proInfo', params: { id:`${item.id}`}}">
+             v-for="item in lfLists">
+            <router-link :to="{ name: 'proInfo', params: { id:`${item.brandId}`}}">
                 <img class="width185"
                      v-lazy.container="'http://www.hykeyun.com/image?u=' + item.image">
                 <div class="title">{{item.title}}</div>
+                <div class="flexBox flex-row flex-main-between f12 p10">
+                    <div class="cred">¥388/4天</div>
+                    <div class="gray">市场价¥{{item.sellPrice}}</div>
+                </div>
             </router-link>
         </div>
     </div>
 </template>
 <script>
 export default {
-    props: ['byLists']
+    props: ['lfLists']
 }
 </script>
